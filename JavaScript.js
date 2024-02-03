431715edbb8ef83898e0c1f6a893f9f39617877d
Faabraa <fabramontt@gmail.com>
2024-02-03 23:52:54 +0200

    Function playGame added, now you can play 5 rounds of rock/paper/scissors, and see who won the most

--- JavaScript.js
+++ JavaScript.js
@@ -26,21 +26,50 @@ function getUserSelection() {
     let lowerNumberUser = stringUser.toLowerCase();
     return lowerNumberUser;
 }
-let computer = getComputerChoice();
-let user = getUserSelection();
-console.log("Computer: "+computer);
-console.log("User: " + user);
-let result = playRound(user, computer);
-function playRound(userPick, computerPick)    {
+function playRound(userPick, computerPick) {
     if (userPick === "rock" && computerPick === "scissors") {
         console.log("You won!");
+        return 1;
     } else if (userPick === "paper" && computerPick === "rock") {
         console.log("You won!");
+        return 1;
     } else if (userPick === "scissors" && computerPick === "paper") {
         console.log("You won");
-    } else if (userPick === computerPick)   {
+        return 1;
+    } else if (userPick === computerPick) {
         console.log("You tied");
+        return 0;
     } else {
         console.log("You lost");
+        return -1;
     }
 }
+function playGame() {
+    let computer;
+    let user;
+    let result = 0;
+    let resultSaved = 0;
+    for (let i = 0; i < 5; i++) {
+        computer = getComputerChoice();
+        user = getUserSelection();
+        console.log("Computer: " + computer);
+        console.log("User: " + user);
+        result = playRound(user, computer);
+        if (result === 1)   {
+            resultSaved++;
+        } else if (result === -1)   {
+            resultSaved--;
+        }
+    }
+    testResult(resultSaved)
+}
+function testResult (Result)  {
+    if(Result > 0)  {
+        console.log("Congrats you won the game!")
+    } else if (Result < 0)  {
+        console.log("Unfortunately you lost the game :(")
+    } else {
+        console.log("That was close, but ended on a tie")
+    }
+}
+playGame();
